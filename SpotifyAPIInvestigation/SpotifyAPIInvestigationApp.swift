eb53e7b71c1982ccf8b127524e1ce470db11138e
osanaikoutarou <osanai.biz@gmail.com>
2024-06-24 07:33:14 +0900

    Initial Commit

---
+++ SpotifyAPIInvestigation/SpotifyAPIInvestigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyAPIInvestigationApp.swift
+//  SpotifyAPIInvestigation
+//
+//  Created by 長内幸太郎 on 2024/06/24.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyAPIInvestigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
